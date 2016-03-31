d100d7fdf7cee8d167fb9e610d000efe55a4278e
Valep42 <p.czempin@gmx.de>
2016-04-01 01:37:23 +0200

    added simple tile selection

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	private GameObject selectedTile;
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log("test");
+			Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+			foreach (Transform objectTransform in GetComponentInChildren<Transform>()) {
+				GameObject gameObject = objectTransform.gameObject;
+				Debug.Log(gameObject.GetComponent<BoxCollider2D>().bounds);
+				if (gameObject.GetComponent<BoxCollider2D>().bounds.Contains((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition))) {
+					selectedTile = gameObject;
+				}
+			}
+		}
+
+		if(selectedTile != null) {
+			GameObject selection = GameObject.FindWithTag("Selection");
+			selection.SetActive(true);
+			selection.transform.position = Camera.main.WorldToScreenPoint(selectedTile.transform.position);
+		}
+	}
+}
