747731da3e1877e7f8a5122021b7303f9244768d
Valep42 <p.czempin@gmx.de>
2016-04-01 00:02:52 +0200

    added camera movement with keyboard

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private Camera camera;
+	// Use this for initialization
+	void Start () {
+		camera = GetComponent<Camera>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int horizontal = 0;
+		int vertical = 0;
+
+		horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+		
+		vertical = (int)(Input.GetAxisRaw("Vertical"));
+
+		transform.position += new Vector3(horizontal, vertical, 0);
+	}
+}
